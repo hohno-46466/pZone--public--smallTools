91fa1cceb1bd7dc47a5609982be3d2cb1a5e88a5
hohno-46466 <hohno.46466@gmail.com>
2022-05-01 06:47:35 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- bin/DHdemo.sh
+++ bin/DHdemo.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # First version: Sat Apr 30 08:46:40 JST 2022 by @hohno_at_kuimc
-# Last update: Sat Apr 30 17:45:01 JST 2022 by @Hohno_at_kuimc
+# Prev update: Sat Apr 30 17:45:01 JST 2022 by @hohno_at_kuimc
+# Last update: Sun May  1 06:47:15 JST 2022 by @hohno_at_kuimc
 
 g=2
 
@@ -13,55 +14,73 @@ p=${1-99991}
 #p=${1-999983}
 #p=${1-1000003}
 
-echo "g = $g"
-echo "p = $p"
-echo ""
-
-Rnd1=$(od -An -tu4 -N4 /dev/urandom | tr -d ' ')
-Rnd2=$Rnd1
-
-while [ "$Rnd1" = "$Rnd2" ]; do
-  Rnd2=$(od -An -tu4 -N4 /dev/urandom | tr -d ' ')
-  # echo -n "."
-done
-# echo 
-
-echo "# 乱数発生"
-echo "Rnd1 = $Rnd1"
-echo "Rnd2 = $Rnd2"
-echo ""
-
-Sa=$(($Rnd1 % $p))
-Sb=$(($Rnd2 % $p))
+Sa="$2"
+Sb="$3"
 
 #Sa=22
 #Sb=80
 #Sa=999
 #Sb=1007
 
-echo "# 秘匿鍵を生成"
-echo 'Sa = Rnd1 % p =' "$Rnd1 % $p = $Sa"
-echo 'Sb = Rnd2 % p =' "$Rnd2 % $p = $Sb"
+echo "g = $g"
+echo "p = $p"
 echo ""
 
-Pa=$(calc -- "mod((2 ** $Sa), $p)" | tr -d ' 	')
-Pb=$(calc -- "mod((2 ** $Sb), $p)" | tr -d ' 	')
+if [ "x$2" != "x" -a "x$3" != "x" ]; then
+
+  Sa=$(($Sa % $p))
+  Sb=$(($Sb % $p))
+
+  echo "# 秘匿鍵を決定"
+  echo "Sa = $Sa"
+  echo "Sb = $Sb"
+  echo ""
+
+else
+
+  Rnd1=$(od -An -tu4 -N4 /dev/urandom | tr -d ' ')
+  Rnd2=$Rnd1
+  
+  while [ "$Rnd1" = "$Rnd2" ]; do
+    Rnd2=$(od -An -tu4 -N4 /dev/urandom | tr -d ' ')
+    # echo -n "."
+  done
+  # echo 
+  
+  echo "# 乱数発生"
+  echo "Rnd1 = $Rnd1"
+  echo "Rnd2 = $Rnd2"
+  echo ""
+  
+  Sa=$((($Rnd1 % ($p-1)) + 1))
+  Sb=$((($Rnd2 % ($p-1)) + 1))
+
+  echo "# 秘匿鍵を生成"
+  echo "Sa = Rnd1 mod p = ($Rnd1 mod ($p-1))+1 = $Sa  # Note: Sa = 1..p-1"
+  echo "Sb = Rnd2 mod p = ($Rnd2 mod ($p-1))+1 = $Sb  # Note: Sb = 1..p-1"
+  echo ""
+
+fi
+
+
+Pa=$(calc -- "(2 ** $Sa) % $p" | tr -d ' 	')
+Pb=$(calc -- "(2 ** $Sb) % $p" | tr -d ' 	')
 
 echo "# 公開鍵を生成"
-echo 'Pa = mod((g^Sa),p) =' "mod((2^$Sa),$p) = $Pa"
-echo 'Pb = mod((g^Sb),p) =' "mod((2^$Sb),$p) = $Pb"
+echo "Pa = g^Sa mod p = 2^$Sa mod $p = $Pa"
+echo "Pb = g^Sb mos p = 2^$Sb mod $p = $Pb"
 echo ""
 
-Ka=$(calc -- "mod(($Pb ** $Sa), $p)" | tr -d ' 	')
-Kb=$(calc -- "mod(($Pa ** $Sb), $p)" | tr -d ' 	')
+Ka=$(calc -- "($Pb ** $Sa) % $p" | tr -d ' 	')
+Kb=$(calc -- "($Pa ** $Sb) % $p" | tr -d ' 	')
 
 echo "# 相手の公開鍵と自分の秘匿鍵から共有鍵を生成"
-echo 'Ka = mod((Pb^Sa),p)' "= mod(($Pb^$Sa),$p) = $Ka"
-echo 'Kb = mod((Pa^Sb),p)' "= mod(($Pa^$Sb),$p) = $Kb"
+echo "Ka = Pb^Sa mod p = $Pb^$Sa mod $p = $Ka"
+echo "Kb = Pa^Sb mod p = $Pa^$Sb mod $p = $Kb"
 echo ""
 
-Kcommon=$(calc -- "mod((2 ** ($Sa * $Sb)), $p)" 2>&1 | expand | sed 's/^ *//')
+Kcommon=$(calc -- "(2 ** ($Sa * $Sb)) % $p" 2>&1 | expand | sed 's/^ *//')
 
 echo "# 共有鍵を確認（Sa*Sb の大きさによってはエラーになることもある）"
-echo 'Kcommon = mod((2^(Sa*Sb)),p)' "= mod((2^($Sa*$Sb)),$p) = $Kcommon"
+echo "Kcommon = 2^(Sa*Sb) mod p = 2^($Sa*$Sb) mod $p = $Kcommon"
 echo ""

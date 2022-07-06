5236fe237098016a9898134f48c181ea62e53ee0
hohno-46466 <hohno.46466@gmail.com>
2022-07-06 13:01:47 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- bin/addts.sh
+++ bin/addts.sh
@@ -2,4 +2,7 @@
 export date=date; type gdate > /dev/null 2>&1 && date=gdate # alias date=gdate
 export sed=sed;   type gsed  > /dev/null 2>&1 && sed=gsed   # alias sed=gsed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
-cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | /bin/sh
+#cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | egrep -u '^echo' | /bin/sh
+#cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | egrep -u '^echo' | /bin/sh
+cat -u "$@" | $sed -u -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t"/' | /bin/sh
+exit

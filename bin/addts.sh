0342ca6d49503b7feb97f756642ee29f80cfc98a
hohno-46466 <hohno.46466@gmail.com>
2022-07-08 07:12:36 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- bin/addts.sh
+++ bin/addts.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-export date=date; type gdate > /dev/null 2>&1 && date=gdate # alias date=gdate
-export sed=sed;   type gsed  > /dev/null 2>&1 && sed=gsed   # alias sed=gsed
+type gdate > /dev/null 2>&1 || gdate=date # alias gdate=date
+type gsed  > /dev/null 2>&1 || gsed=sed   # alias gsed=sed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
-#cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | egrep -u '^echo' | /bin/sh
-#cat "$@" | $sed -u -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t/' -e 's/$/"/' | egrep -u '^echo' | /bin/sh
-cat -u "$@" | $sed -u -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$($date +%s.%3N\)\t"/' | /bin/sh
+cat -u "$@" | $gsed -u -e "s/'/'\"'\"'/g" -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$('$gdate' +%s.%3N\)\t"/' | /bin/sh
 exit

be9f10f718b48fcf2b0666213b99f3c3f968a3a4
hohno-46466 <hohno.46466@gmail.com>
2022-07-08 07:18:18 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2016B.local

--- bin/addts.sh
+++ bin/addts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-type gdate > /dev/null 2>&1 || gdate=date # alias gdate=date
-type gsed  > /dev/null 2>&1 || gsed=sed   # alias gsed=sed
+gdate=gdate; type gdate > /dev/null 2>&1 || gdate=date # alias gdate=date
+gsed=gsed;   type gsed  > /dev/null 2>&1 || gsed=sed   # alias gsed=sed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
 cat -u "$@" | $gsed -u -e "s/'/'\"'\"'/g" -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$('$gdate' +%s.%3N\)\t"/' | /bin/sh
 exit

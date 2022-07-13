ac7d4405d18429dca7d24107a69ce660aaf38f49
hohno-46466 <hohno.46466@gmail.com>
2022-07-13 10:23:32 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- bin/ghcg2.sh
+++ bin/ghcg2.sh
@@ -19,10 +19,11 @@ rm -f ${READMEnew} || exit 2
 
 cat ${READMEold} | sed -n -e "1,/.\/$zone/p" | sed '$d' >> ${READMEnew}
 
-(cd ..; for x in ${zone}*; do echo $x 1>&2; cd $x; treex -L 3; cd ..; echo; done) >> ${READMEnew}
+(cd ..; for x in ${zone}*; do echo $x 1>&2; cd $x; n=$(treex -L 4 | wc -l); if [ $n -gt 50 ]; then treex -L 3; else echo $(treex -L 4|wc -l); fi; cd .. ; echo; done
 
 # (echo; (cat ${READMEold} | sed -n '/directories/,$p')) | sed '1,/directories/d' >> ${READMEnew}
-cat ${READMEold} | sed -n '/^Note:/,$p' >>  ${READMEnew}
+# cat ${READMEold} | sed -n '/^Note:/,$p' >>  ${READMEnew}
+tac ${READMEold} | sed -n -e '1,/director.*JST 20[0-9][0-9]/p'| tac | tail +2 >> ${READMEnew}
 
 exit $?
 

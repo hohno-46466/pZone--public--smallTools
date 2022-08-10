df8335ae71877046f2e8e229fc1beef5d5f29931
hohno-46466 <hohno.46466@gmail.com>
2022-08-10 20:54:23 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- bin/addts.sh
+++ bin/addts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-# Last update: Fri Jul  8 19:16:09 JST 2022
-date=date; type gdate > /dev/null 2>&1 && date=gdate # alias date=gdate
+# Prev modified: Fri Jul  8 19:16:09 JST 2022
+# Last modified: Wed Aug 10 20:53:13 JST 2022
+date=date; type gdate > /dev/null 2>&1 && date=gdate # alias date=gdate # On Mac, gdate can be found in coreutils
 sed=sed;   type gsed  > /dev/null 2>&1 && sed=gsed   # alias sed=gsed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
 cat -u "$@" | $sed -u -e "s/'/'\"'\"'/g" -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$('$date' +%s.%3N\)\t"/' | /bin/sh

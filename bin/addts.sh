fc01e2e28b4aa3dc10e64cb0a846f30575bab394
hohno-46466 <hohno.46466@gmail.com>
2022-08-11 12:00:35 +0900

    updated by @hohno_at_kuimc on

--- bin/addts.sh
+++ bin/addts.sh
@@ -5,3 +5,10 @@ sed=sed;   type gsed  > /dev/null 2>&1 && sed=gsed   # alias sed=gsed
 [ "x$TSKEY" = "x" ] && TSKEY='$USER-$$'
 cat -u "$@" | $sed -u -e "s/'/'\"'\"'/g" -e "s/^/'/" -e "s/$/'/" -e 's/^/echo "'$TSKEY'\t$('$date' +%s.%3N\)\t"/' | /bin/sh
 exit $?
+
+# 上記の sed の補足説明
+# -e "s/'/'\"'\"'/g"  - 「'」を「'"'"'」に置き換える
+# -e "s/^/'/"         - 行頭に「'」を付加する．直上の変更と組み合わせると入力行の「'」が「"'"」になる
+# -e "s/$/'/"         - 行末に「'」を付加する
+# -e 's/^/echo "'$TSKEY'\t$('$date' +%s.%3N\)\t"/' - 上記の行の前に「"キーワード(TAB)時刻(TAG)"」を付加する
+

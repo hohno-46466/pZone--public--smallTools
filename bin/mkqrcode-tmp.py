5a09611659eb5cd410da0ebaf25d59e4a888c979
hohno-46466 <hohno.46466@gmail.com>
2022-09-15 07:21:53 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

---
+++ bin/mkqrcode-tmp.py
@@ -0,0 +1,23 @@
+#! /opt/homebrew/opt/coreutils/libexec/gnubin/env python3
+
+# mkqrcode-tmp.py
+
+# First version: Thu Sep 15 07:19:30 JST 2022
+
+import os
+import sys
+import qrcode
+
+HOME=os.environ['HOME'];
+QRcode='https://goo.gl/bgEJB9'
+args=sys.argv
+if (len(args) >= 2): QRcode=args[1]
+print("URL: " + QRcode)
+
+qr = qrcode.QRCode(version=3, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=6, border=4)
+#qr.add_data('https://bit.ly/3bzbn0T')
+qr.add_data(QRcode)
+img = qr.make_image(fill_color="black", back_color="white")
+outfile = HOME + "/tmp/qrsample.gif"
+img.save(outfile)
+print("output: " + outfile)

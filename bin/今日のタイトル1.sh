61ca796aafd26f95a1ec0d9c5acbc1b97c74c571
hohno-46466 <hohno.46466@gmail.com>
2026-06-27 14:32:25 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.me

--- bin/今日のタイトル1.sh
+++ bin/今日のタイトル1.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+# 2026-06-27(Sat) 13:58 JST / 2026-06-27(Sat) 04:58 UTC
+
 DATESTR=$(/bin/date "+%Y-%m-%d %H:%M:%S %Z")
 DATESTR=$(/bin/date "+%Y-%m-%d %Z")
+DATESTR=$(/bin/date "+%Y-%m-%d %H:%M %Z")
 TIMEVAL=$(/bin/date "+%H")
 TIMEVAL=${1:-$TIMEVAL}
 if [ $TIMEVAL -le 3 ]; then x="こんばんは"; 

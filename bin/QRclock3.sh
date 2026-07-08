022b67cf02aa32a4d337d891dba793ecf1da1f3b
hohno-46466 <hohno.46466@gmail.com>
2026-07-08 17:01:59 +0900

    ToDo.md and reference directory have bee newly added by hohno_at_monkKB on hohno-GP4-2026D

--- bin/QRclock3.sh
+++ bin/QRclock3.sh
@@ -2,9 +2,10 @@
 
 # QRclock3.sh
 
-# Last udpate: 2026-07-06(Mon) 20:22 JST / 2026-07-06(Mon) 11:22 UTC
+# Prev udpate: 2026-07-06(Mon) 20:22 JST / 2026-07-06(Mon) 11:22 UTC
+# Last update: 2026-07-08(Wed) 15:25 JST / 2026-07-08(Wed) 06:25 UTC
 
-dateopt="+%Y-%m-%dT%H:%M:%S"
+dateopt="%Y-%m-%dT%H:%M:%S"
 cmd=qrencode
 cmdopts="-t ANSI256 -l H"
 cnt=0
@@ -15,7 +16,8 @@ while [ 1 ]; do
   if [ "x$x" != "x$xx" ]; then
     clear
     $cmd $cmdopts "$x"
-    echo "[ $x ] $cnt"
+    # echo "[ $x ] $cnt"
+    printf "[ %s ] %s" "$x" "$cnt"
     cnt=0
     xx=$x
   fi
